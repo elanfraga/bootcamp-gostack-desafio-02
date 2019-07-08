9e0aa8a390fba17faa1ef63758da45eb226d8359
Elan Fraga <elanfraga@gmail.com>
2019-07-07 23:59:42 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,21 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ erro: ' User already exists.' });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+
+  async update(req, res) {
+    return res.send();
+  }
+}
+
+export default new UserController();
