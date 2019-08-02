336a81b54546c3c8bf35fa08fe354ac2da8f57d2
Elan Fraga <elanfraga@gmail.com>
2019-08-01 22:13:40 -0300

    tratamento de exceção

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
+import 'dotenv/config';
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
 
-import 'dotenv/config';
-
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
